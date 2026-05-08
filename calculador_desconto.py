73ce7a9ba9e9dec3a4f2a52d72b474aa7a21985a
Guilherme Conceicao Ramalho <a2320649@vmpdc.utfpr>
2026-05-08 16:28:52 -0300

    refactor: padrão Strategy aplicado

--- calculador_desconto.py
+++ calculador_desconto.py
@@ -1,28 +1,35 @@
-class SemDesconto:
-    pass
+from abc import ABC, abstractmethod
 
 
-class DescontoPercentual:
+class PoliticaDesconto(ABC):
+    @abstractmethod
+    def aplicar(self, valor_original):
+        pass
+
+
+class SemDesconto(PoliticaDesconto):
+    def aplicar(self, valor_original):
+        return valor_original
+
+
+class DescontoPercentual(PoliticaDesconto):
     def __init__(self, percentual):
         self.percentual = percentual
 
+    def aplicar(self, valor_original):
+        desconto = valor_original * (self.percentual / 100)
+        return valor_original - desconto
 
-class DescontoCupom:
+
+class DescontoCupom(PoliticaDesconto):
     def __init__(self, valor):
         self.valor = valor
+
+    def aplicar(self, valor_original):
+        valor_final = valor_original - self.valor
+        return max(valor_final, 0.0)
 
 
 class CalculadoraDesconto:
     def calcular(self, valor_original, politica):
-        if isinstance(politica, SemDesconto):
-            return valor_original
-
-        if isinstance(politica, DescontoPercentual):
-            desconto = valor_original * (politica.percentual / 100)
-            return valor_original - desconto
-
-        if isinstance(politica, DescontoCupom):
-            valor_final = valor_original - politica.valor
-            return max(valor_final, 0.0)
-
-        raise ValueError("Política de desconto inválida.")
+        return politica.aplicar(valor_original)
